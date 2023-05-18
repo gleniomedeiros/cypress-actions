e789a82bcbb8fd3ebeb0a5d427ecac63cccd2d5a
Glênio Medeiros <glenio28@hotmail.com>
2023-05-17 23:46:59 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'u5x39s',
-  e2e: {
+  projectId:'bau2ch',
+  e2e:{
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    viewportWidth: 1280,
-    viewportHeight: 720,
+    viewportWidth:1280,
+    viewportHeight:720,
     baseUrl: "https://loginxp.vercel.app"
   }
 });
